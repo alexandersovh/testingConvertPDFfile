aba9dad8c1b6da516f5483d6ce6ab40caf1d05c0
Alexandr <alexandrsivoh@gmail.com>
2023-01-11 22:32:41 +0300

    Add project files.

---
+++ testingConvertPDFfile/PDFToString.cs
@@ -0,0 +1,31 @@
+﻿using iTextSharp.text.pdf.parser;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using System.IO;
+
+namespace testingConvertPDFfile
+{
+    internal class PDFToString
+    {
+        public string ReadPdfFile(string pathPDF)
+        {
+            StringBuilder text = new StringBuilder();
+
+            PdfReader pdfReader = new PdfReader(pathPDF);
+
+            ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+            string currentText = PdfTextExtractor.GetTextFromPage(pdfReader, 1, strategy);
+
+            return currentText;
+        }
+        public void NewPDFReadr(string fileName)
+        {
+
+        }
+    }
+}
---
+++ testingConvertPDFfile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.Threading.Tasks;
+
+
+namespace testingConvertPDFfile
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var prog = new Program();
+            var testObj = new PDFToString();
+
+
+            //Документ Microsoft Word.pdf//Заявление ПД221018045.pdf
+            string path = "C:\\Users\\alexandr\\OneDrive\\Рабочий стол\\лаборатрория\\Заявление ПЖ221026008adob.pdf";
+            string pathTo = "C:\\Users\\alexandr\\OneDrive\\Рабочий стол\\лаборатрория\\output1.txt";
+
+            string result = testObj.ReadPdfFile(path);
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
+
