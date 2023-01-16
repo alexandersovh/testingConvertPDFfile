f2f1963b9060a908bbfc1559bacf751997e78f38
alexandersovh <alexandrsivoh@gmail.com>
2023-01-16 16:01:58 +0300

    попытка в rtf cheking

--- testingConvertPDFfile/Program.cs
+++ testingConvertPDFfile/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.IO;
 using System.Threading.Tasks;
-using iText.Layout;
-using iText.Layout.Element;
-using iText.Kernel.Pdf;
-using iText.Layout.Properties;
+using RtfPipe;
+
 
 namespace testingConvertPDFfile
 {
@@ -20,27 +16,28 @@ namespace testingConvertPDFfile
 
 
             //Документ Microsoft Word.pdf//Заявление ПД221018045.pdf
-            string path = "C:\\Users\\alexandr\\OneDrive\\Рабочий стол\\лаборатрория\\Заявление ПЖ221026008adob.pdf";
+            string path = "C:\\Users\\aa.sivohin\\Desktop\\лаба\\Заявление ЮБ230106001.pdf";
             string pathTo = "C:\\Users\\alexandr\\OneDrive\\Рабочий стол\\лаборатрория\\output1.txt";
 
             string result = testObj.ReadPdfFile(path);
             Console.WriteLine(result);
             Console.ReadKey();
         }
-        static void ConvOnTtext(string path)
+        public string Convert(Stream stream)
         {
-            // Must have write permissions to the path folder
-            PdfWriter writer = new PdfWriter(path);
-            PdfDocument pdf = new PdfDocument(writer);
-            Document document = new Document(pdf);
-            Paragraph header = new Paragraph("HEADER")
-               .SetTextAlignment(TextAlignment.CENTER)
-               .SetFontSize(20);
-
-            document.Add(header);
-            document.Close();
+            stream.Position = 0;
+            string rtf = string.Empty;
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                rtf = sr.ReadToEnd();
+            }
+            // Эта строчка необходима для работы RtfPipe в Core 
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            // С помощью либы RtfPipe создаем html
+            var html = Rtf.ToHtml(rtf);
+            // очищаем html от лишних тегов и атрибутов, возвращем готовую xml
+            return ClearHtml(html);
         }
-
     }
 }
 
